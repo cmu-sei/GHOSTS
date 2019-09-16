38d3e76443270ee379769cf1531de417976041c6
sei-dupdyke <ddupdyke@sei.cmu.edu>
2019-09-16 08:26:40 -0400

    Corrects bug #20

--- Ghosts.Domain/Code/WorkingHours.cs
+++ Ghosts.Domain/Code/WorkingHours.cs
@@ -15,39 +15,43 @@ namespace Ghosts.Domain.Code
 
         public static void Is(TimelineHandler handler)
         {
-            if (handler.UtcTimeOn > new TimeSpan(0, 0, 0) && handler.UtcTimeOff > new TimeSpan(0, 0, 0)) //ignore the unset
+            var timeOn = handler.UtcTimeOn;
+            var timeOff = handler.UtcTimeOff;
+
+            if (timeOn > new TimeSpan(0, 0, 0) && timeOff > new TimeSpan(0, 0, 0)) //ignore the unset
             {
                 //fuzz
                 var r = new Random().Next(-30, 30);
                 if (r > 0)
-                    handler.UtcTimeOn = handler.UtcTimeOn.Add(new TimeSpan(0, r, new Random().Next(0, 59)));
+                    timeOn = timeOn.Add(new TimeSpan(0, r, new Random().Next(0, 59)));
                 else
-                    handler.UtcTimeOn = handler.UtcTimeOn.Subtract(new TimeSpan(0, r, new Random().Next(0, 59)));
+                    timeOn = timeOn.Subtract(new TimeSpan(0, r, new Random().Next(0, 59)));
 
                 r = new Random().Next(-30, 30);
                 if (r > 0)
-                    handler.UtcTimeOff = handler.UtcTimeOff.Add(new TimeSpan(0, new Random().Next(-30, 30), new Random().Next(0, 59)));
+                    timeOff = timeOff.Add(new TimeSpan(0, new Random().Next(-30, 30), new Random().Next(0, 59)));
                 else
-                    handler.UtcTimeOff = handler.UtcTimeOff.Subtract(new TimeSpan(0, new Random().Next(-30, 30), new Random().Next(0, 59)));
+                    timeOff = timeOff.Subtract(new TimeSpan(0, new Random().Next(-30, 30), new Random().Next(0, 59)));
 
-                var isOvernight = handler.UtcTimeOff < handler.UtcTimeOn;
+                var isOvernight = timeOff < timeOn;
 
-                _log.Debug($"For {handler.HandlerType}: Local time: {DateTime.Now.TimeOfDay} UTC: {DateTime.UtcNow.TimeOfDay} On: {handler.UtcTimeOn} Off: {handler.UtcTimeOff} Overnight? {isOvernight}");
+                _log.Debug($"For {handler.HandlerType}: Local time: {DateTime.Now.TimeOfDay} UTC: {DateTime.UtcNow.TimeOfDay} On: {timeOn} Off: {timeOff} Overnight? {isOvernight}");
 
                 if (isOvernight)
                 {
-                    while (DateTime.UtcNow.TimeOfDay < handler.UtcTimeOn)
+                    while (DateTime.UtcNow.TimeOfDay < timeOn)
                     {
-                        var sleep = Math.Abs((handler.UtcTimeOn - DateTime.UtcNow.TimeOfDay).TotalMilliseconds);
+                        var sleep = Math.Abs((timeOn - DateTime.UtcNow.TimeOfDay).TotalMilliseconds);
+                        if (sleep > 300000)
+                            sleep = 300000;
                         _log.Trace($"Sleeping for {sleep}");
-                        //delay until start time
                         Thread.Sleep(Convert.ToInt32(sleep));
                     }
                 }
                 else
                 {
-                    while (DateTime.UtcNow.TimeOfDay < handler.UtcTimeOn ||
-                           DateTime.UtcNow.TimeOfDay > handler.UtcTimeOff)
+                    while (DateTime.UtcNow.TimeOfDay < timeOn ||
+                           DateTime.UtcNow.TimeOfDay > timeOff)
                     {
                         _log.Trace($"Sleeping for 60000");
                         Thread.Sleep(60000);
