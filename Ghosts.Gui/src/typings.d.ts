5c5e8e9bb59698f6a14617d684efbe7da3c2c9d9
sei-dupdyke <ddupdyke@sei.cmu.edu>
2019-09-16 08:29:12 -0400

    Removes gui project

--- Ghosts.Gui/src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 Carnegie Mellon University. All Rights Reserved. See LICENSE.md file for terms.
-
-/* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
