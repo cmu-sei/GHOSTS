2564a2c1e90d869543fef75644d7db5dbeb82475
sei-dupdyke <ddupdyke@sei.cmu.edu>
2022-08-01 13:55:40 -0400

    Adds force download for pdfs instead of letting them display directly in the browser (which often causes perf issues)

---
+++ src/Ghosts.Client/Infrastructure/KnownFolders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ghosts.Client.Infrastructure
+{
+    public static class KnownFolders
+    {
+        public static string GetHomePath()
+        {
+            return Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+        }
+
+
+        public static string GetDownloadFolderPath()
+        {
+            return GetHomePath() + "\\Downloads";
+        }
+    }
+}
