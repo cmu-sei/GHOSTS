6cb1b43fabef645ea5c156b4bd9420d02b728af4
sei-dupdyke <ddupdyke@sei.cmu.edu>
2022-08-26 08:43:06 -0400

    general clean up

--- src/Ghosts.Domain/Code/Helpers/ProcessExtentions.cs
+++ src/Ghosts.Domain/Code/Helpers/ProcessExtentions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright 2017 Carnegie Mellon University. All Rights Reserved. See LICENSE.md file for terms.
 
 using System.Diagnostics;
-using NLog;
 
 namespace Ghosts.Domain.Code.Helpers
 {
