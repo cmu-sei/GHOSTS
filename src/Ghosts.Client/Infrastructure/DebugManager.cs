ff2c7b18a6cf5680cfe876e2782495f3ce8e91ef
sei-dupdyke <ddupdyke@sei.cmu.edu>
2023-02-09 18:01:34 -0500

    adds better debugging for what the client checkin information

--- src/Ghosts.Client/Infrastructure/DebugManager.cs
+++ src/Ghosts.Client/Infrastructure/DebugManager.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Security.Principal;
+using Ghosts.Domain;
 using Ghosts.Domain.Code;
+using NLog;
 
 namespace Ghosts.Client.Infrastructure;
 
 internal static class DebugManager
 {
+    public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
     public static void Evaluate()
     {
         var mode = "production";
@@ -14,21 +18,42 @@ internal static class DebugManager
         {
             mode = "debug";
         }
-        Console.WriteLine($"GHOSTS ({ApplicationDetails.Name}:{ApplicationDetails.Version} [{ApplicationDetails.VersionFile}]) running in {mode} mode");
-        Console.WriteLine($"Installed path: {ApplicationDetails.InstalledPath}");
-        Console.WriteLine($"Running as Username: {Environment.UserName} - WindowsIdentity: {WindowsIdentity.GetCurrent().Name}");
-        Console.WriteLine($"{ApplicationDetails.ConfigurationFiles.Application} == {File.Exists(ApplicationDetails.ConfigurationFiles.Application)}");
-        Console.WriteLine($"{ClientConfigurationResolver.Dictionary} == {File.Exists(ClientConfigurationResolver.Dictionary)}");
-        Console.WriteLine($"{ClientConfigurationResolver.EmailContent} == {File.Exists(ClientConfigurationResolver.EmailContent)}");
-        Console.WriteLine($"{ClientConfigurationResolver.EmailReply} == {File.Exists(ClientConfigurationResolver.EmailReply)}");
-        Console.WriteLine($"{ClientConfigurationResolver.EmailDomain} == {File.Exists(ClientConfigurationResolver.EmailDomain)}");
-        Console.WriteLine($"{ClientConfigurationResolver.EmailOutside} == {File.Exists(ClientConfigurationResolver.EmailOutside)}");
-        Console.WriteLine($"{ApplicationDetails.ConfigurationFiles.Health} == {File.Exists(ApplicationDetails.ConfigurationFiles.Health)}");
-        Console.WriteLine($"{ApplicationDetails.ConfigurationFiles.Timeline} == {File.Exists(ApplicationDetails.ConfigurationFiles.Timeline)}");
-        Console.WriteLine($"{ApplicationDetails.InstanceFiles.Id} == {File.Exists(ApplicationDetails.InstanceFiles.Id)}");
-        Console.WriteLine($"{ApplicationDetails.InstanceFiles.FilesCreated} == {File.Exists(ApplicationDetails.InstanceFiles.FilesCreated)}");
-        Console.WriteLine($"{ApplicationDetails.InstanceFiles.Trackables} == {File.Exists(ApplicationDetails.InstanceFiles.Trackables)}");
-        Console.WriteLine($"{ApplicationDetails.InstanceFiles.SurveyResults} == {File.Exists(ApplicationDetails.InstanceFiles.SurveyResults)}");
-        Console.WriteLine($"{ApplicationDetails.LogFiles.ClientUpdates} == {File.Exists(ApplicationDetails.LogFiles.ClientUpdates)}");
+        Write($"GHOSTS ({ApplicationDetails.Name}:{ApplicationDetails.Version} [{ApplicationDetails.VersionFile}]) running in {mode} mode");
+        Write($"Installed path: {ApplicationDetails.InstalledPath}");
+        Write($"Running as Username: {Environment.UserName} - WindowsIdentity: {WindowsIdentity.GetCurrent().Name}");
+        Write($"{ApplicationDetails.ConfigurationFiles.Application} == {File.Exists(ApplicationDetails.ConfigurationFiles.Application)}");
+        Write($"{ClientConfigurationResolver.Dictionary} == {File.Exists(ClientConfigurationResolver.Dictionary)}");
+        Write($"{ClientConfigurationResolver.EmailContent} == {File.Exists(ClientConfigurationResolver.EmailContent)}");
+        Write($"{ClientConfigurationResolver.EmailReply} == {File.Exists(ClientConfigurationResolver.EmailReply)}");
+        Write($"{ClientConfigurationResolver.EmailDomain} == {File.Exists(ClientConfigurationResolver.EmailDomain)}");
+        Write($"{ClientConfigurationResolver.EmailOutside} == {File.Exists(ClientConfigurationResolver.EmailOutside)}");
+        Write($"{ApplicationDetails.ConfigurationFiles.Health} == {File.Exists(ApplicationDetails.ConfigurationFiles.Health)}");
+        Write($"{ApplicationDetails.ConfigurationFiles.Timeline} == {File.Exists(ApplicationDetails.ConfigurationFiles.Timeline)}");
+        Write($"{ApplicationDetails.InstanceFiles.Id} == {File.Exists(ApplicationDetails.InstanceFiles.Id)}");
+        Write($"{ApplicationDetails.InstanceFiles.FilesCreated} == {File.Exists(ApplicationDetails.InstanceFiles.FilesCreated)}");
+        Write($"{ApplicationDetails.InstanceFiles.Trackables} == {File.Exists(ApplicationDetails.InstanceFiles.Trackables)}");
+        Write($"{ApplicationDetails.InstanceFiles.SurveyResults} == {File.Exists(ApplicationDetails.InstanceFiles.SurveyResults)}");
+        Write($"{ApplicationDetails.LogFiles.ClientUpdates} == {File.Exists(ApplicationDetails.LogFiles.ClientUpdates)}");
+
+        var machine = new ResultMachine();
+        GuestInfoVars.Load(machine);
+        Write("------------------");
+        Write("Client Check-in Values:");
+        Write($"Machine.Id = {machine.Id}");
+        Write($"Machine.Name = {machine.Name}");
+        Write($"Machine.CurrentUsername = {machine.CurrentUsername}");
+        Write($"Machine.Domain = {machine.Domain}");
+        Write($"Machine.ClientIp = {machine.ClientIp}");
+        Write($"Machine.FQDN = {machine.FQDN}");
+        Write($"Machine.Host = {machine.Host}");
+        Write($"Machine.IpAddress = {machine.IpAddress}");
+        Write($"Machine.ResolvedHost = {machine.ResolvedHost}");
+        Write("------------------");
+    }
+
+    private static void Write(string line)
+    {
+        Log.Info(line);
+        Console.WriteLine(line);
     }
 }
