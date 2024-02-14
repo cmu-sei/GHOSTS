390adc28c980377221e4fabe5cc455a61a1e8252
sei-dupdyke <ddupdyke@sei.cmu.edu>
2024-02-14 18:24:40 -0500

    version bump

--- src/Ghosts.Client/Properties/AssemblyInfo.cs
+++ src/Ghosts.Client/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("8.0.0.0")]
-[assembly: AssemblyInformationalVersion("7.1.0.0")]
+[assembly: AssemblyInformationalVersion("8.0.0.0")]
 [assembly: AssemblyFileVersion("8.0.0.0")]
