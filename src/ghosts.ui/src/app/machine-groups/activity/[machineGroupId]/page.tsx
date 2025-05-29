82b47c80d550d041b9950bfe8e384ae87fd7a377
Dustin Updyke <ddupdyke@sei.cmu.edu>
2025-05-29 18:00:04 -0400

    drizzle update

--- src/ghosts.ui/src/app/machine-groups/activity/[machineGroupId]/page.tsx
+++ src/ghosts.ui/src/app/machine-groups/activity/[machineGroupId]/page.tsx
@@ -3,20 +3,24 @@ import { SheetWrapper } from "@/components/sheet-wrapper";
 import { api } from "@/generated/endpoints";
 import { getSortedActivity } from "@/lib/server-utils";
 
-export default async function Page({
-	params: { machineGroupId },
-	searchParams,
-}: { params: { machineGroupId: string }; searchParams: unknown }) {
-	const id = Number.parseInt(machineGroupId);
+type PageProps = {
+	params: { machineGroupId: string };
+	searchParams?: Record<string, string | string[]>;
+};
+
+export default async function Page({ params, searchParams }: PageProps) {
+	const id = Number.parseInt(params.machineGroupId);
+
 	const sortedActivity = await getSortedActivity(
 		api.getApimachinegroupsIdactivity,
 		id,
 		searchParams,
-		`/machine-groups/activity/${machineGroupId}?skip=0&take=50`,
+		`/machine-groups/activity/${params.machineGroupId}?skip=0&take=50`
 	);
 
 	const machineGroup = await api.getApimachinegroupsId({ params: { id } });
 	const allMachines = await api.getApimachines();
+
 	return (
 		<SheetWrapper returnPath="/machine-groups" side="right">
 			<ActivitySheet
