1743ed3dc0b0bd423dce8e187557196cc1439d0a
Dustin Updyke <ddupdyke@sei.cmu.edu>
2025-05-29 17:34:45 -0400

    drizzle update

--- src/ghosts.ui/drizzle.config.ts
+++ src/ghosts.ui/drizzle.config.ts
@@ -1,10 +1,12 @@
-import { DB_URL } from "@/lib/db";
+// drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
 
+export const DB_URL = "sqlite.db";
+
 export default defineConfig({
-	schema: "./src/lib/db.ts",
-	driver: "better-sqlite",
-	dbCredentials: {
-		url: DB_URL,
-	},
+  schema: "./src/lib/db.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: DB_URL,
+  },
 });
