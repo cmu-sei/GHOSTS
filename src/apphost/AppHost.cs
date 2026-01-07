549dd5bb3eb5028076c16166445306aa55fae448
Dustin Updyke <ddupdyke@sei.cmu.edu>
2026-01-07 20:50:09 +0000

    devcontainer fixes

--- src/apphost/AppHost.cs
+++ src/apphost/AppHost.cs
@@ -1,20 +1,31 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres")
+var postgresUsername = builder.AddParameter("PostgresUsername", "postgres", false);
+var postgresPassword = builder.AddParameter("PostgresPassword", "postgres", false);
+
+var postgres = builder.AddPostgres("postgres", userName: postgresUsername, password: postgresPassword)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithContainerName("ghosts-postgres")
     .WithPgAdmin(pgAdmin =>
     {
-        pgAdmin.WithLifetime(ContainerLifetime.Persistent);
+        pgAdmin.WithEndpoint("http", endpoint => endpoint.Port = 33000);
     });
 
 var db = postgres.AddDatabase("ghosts", "ghosts");
 
-var api = builder.AddProject<Projects.Ghosts.Api>("api")
+var api = builder.AddProject<Projects.Ghosts_Api>("api")
     .WaitFor(postgres)
-    .WithHttpHealthCheck("api")
     .WithReference(db, "DefaultConnection")
     .WithEnvironment("ConnectionStrings__Provider", "PostgreSQL");
+
+var frontend = builder.AddJavaScriptApp("frontend", "../ghosts.ng", "start")
+    .WithHttpEndpoint(port: 4200, env: "PORT", isProxied: false)
+    .WithUrlForEndpoint("http", url =>
+    {
+        url.DisplayText = "Ghosts UI";
+        url.Url = "http://localhost:4200/";
+    });
+//.WithNpmPackageInstallation();
 
 builder.Build().Run();
