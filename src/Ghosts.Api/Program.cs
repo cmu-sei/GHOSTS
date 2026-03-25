d0b299aae5c130ff3464363f0464098387694676
Nicholas Blair (wsl@nblair2-43464s) <nicholas.blair@nrel.gov>
2026-03-25 10:32:53 -0600

    fix(api): I fixed something

--- src/Ghosts.Api/Program.cs
+++ src/Ghosts.Api/Program.cs
@@ -46,6 +46,7 @@ public class Program
         var msg = $"GHOSTS API {ApplicationDetails.Version} ({ApplicationDetails.VersionFile}) coming online...";
         Console.WriteLine(msg);
         _log.Info(msg);
+        _log.Info("I am fixing something");
 
         ApiDetails.LoadConfiguration();
 
